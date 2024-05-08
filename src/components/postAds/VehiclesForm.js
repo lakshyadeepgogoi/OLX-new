9f27fb499bd111888847b3ce1644be2082b4044b
lakshyadeepgogoi <lakshyadeep19@gmail.com>
2024-05-09 00:54:34 +0530

    vehicleform updated

--- src/components/postAds/VehiclesForm.js
+++ src/components/postAds/VehiclesForm.js
@@ -1,14 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react'
 import { Checkbox } from 'flowbite-react';
+import { useState } from 'react';
+import { auth, db } from '../../pages/firebase';
 import { useNavigate } from 'react-router-dom';
-import { db } from '../../pages/firebase';  // Ensure this path is correct for your Firebase setup
-import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import { getFirestore, getDocs, collection, addDoc, serverTimestamp, doc, setDoc } from 'firebase/firestore';
+import { getStorage, ref, uploadBytes } from 'firebase/storage';
+import { getDownloadURL } from 'firebase/storage';
 
-function VehiclesForm({ nextStep }){
+function VehiclesForm({ nextStep, previousStep, selectedCategory }) {
     const [subcategory, setSubcategory] = useState('');
     const [brandOptions, setBrandOptions] = useState([]);
     const [brand, setBrand] = useState('');
     const [model, setModel] = useState('');
+    const [totalkm, setTotalkm] = useState('');
     const [price, setPrice] = useState('');
     const [negotiable, setNegotiable] = useState('');
     const [description, setDescription] = useState('');
@@ -18,16 +22,17 @@ function VehiclesForm({ nextStep }){
 
     const [error, setError] = useState('');
     const [isSubmitting, setIsSubmitting] = useState(false);
-
-    const [collectionId, setCollectionId] = useState('');
     const [documentId, setDocumentId] = useState('');
+    const [collectionId, setCollectionId] = useState('');
+    const [images, setImages] = useState(Array(6).fill(null));
+    const [imageLoadings, setImageLoadings] = useState(Array(6).fill(false));
 
     const navigate = useNavigate();
 
     const handleSubcategoryChange = (event) => {
         const selectedSubcategory = event.target.value;
         setSubcategory(selectedSubcategory);
-        
+
         switch (selectedSubcategory) {
             case 'Bike':
                 setBrandOptions(['Harley Davidson', 'Yamaha', 'Honda', 'Suzuki']);
@@ -45,7 +50,7 @@ function VehiclesForm({ nextStep }){
     };
 
     const handleBrandChange = (event) => {
-        const  selectedBrand = event.target.value;
+        const selectedBrand = event.target.value;
         setBrand(event.target.value);
     };
 
@@ -68,30 +73,76 @@ function VehiclesForm({ nextStep }){
     const handleAdNameChange = (event) => {
         setAdName(event.target.value);
     };
+    const handletotalKmChange = (event) => {
+        setTotalkm(event.target.value);
+    };
 
     const handleAddTag = () => {
-        if (newTag && !tags.includes(newTag) && tags.length < 5) {
-            setTags([...tags, newTag]);
+        if (tags.length < 5 && newTag.trim() !== '') {
+            setTags(prevTags => [...prevTags, newTag.trim()]);
             setNewTag('');
         }
     };
+
 
     const handleRemoveTag = (tagToRemove) => {
         setTags(tags.filter(tag => tag !== tagToRemove));
     };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        if (!subcategory || !brand || !model || !adName || !price || !negotiable || !description) {
-            setError('All fields are required.');
-            return;
+    const handleImageChange = async (e, index) => {
+        const file = e.target.files[0];
+        if (file) {
+            setImageLoadings(prev => prev.map((loading, idx) => idx === index ? true : loading));
+
+            const storage = getStorage();
+            const storageRef = ref(storage, `AdImages/${file.name}`);
+
+            try {
+                await uploadBytes(storageRef, file);
+                const downloadURL = await getDownloadURL(storageRef);
+
+                setImages(prevImages => {
+                    const newImages = [...prevImages];
+                    newImages[index] = downloadURL;
+                    return newImages;
+                });
+                setImageLoadings(prev => prev.map((loading, idx) => idx === index ? false : loading));
+            } catch (error) {
+                console.error('Error uploading image:', error);
+                setImageLoadings(prev => prev.map((loading, idx) => idx === index ? false : loading));
+            }
         }
+    };
 
-        setIsSubmitting(true);
+    const handlePreviousClick = async (event) => {
+        previousStep();
+    };
 
+    if (!selectedCategory) {
+        throw new Error('Category is required.');
+    }
+
+    const handleNextClick = async (event) => {
+        event.preventDefault();
         try {
-            const vehiclesCollection = collection(db, "vehicles");
-            const vehicleDocRef = await addDoc(vehiclesCollection, {
+            if (!subcategory || !brand || !model || !adName || !price || !negotiable || !description) {
+                setError('All fields are required.');
+                return;
+            }
+
+            setIsSubmitting(true);
+            const userId = auth.currentUser.uid
+            if (!userId) {
+                throw new Error('User ID not found.');
+            }
+
+
+
+            const categoryRef = doc(db, 'categories', selectedCategory);
+            const adsCollectionRef = collection(categoryRef, 'ads');
+            const vehicleDocRef = await addDoc(adsCollectionRef, {
+                selectedCategory,
+                userId,
                 subcategory,
                 brand,
                 model,
@@ -99,22 +150,24 @@ function VehiclesForm({ nextStep }){
                 tags,
                 description,
                 price,
+                totalkm,
                 negotiable,
+                images: images.filter(image => image !== null),
                 timestamp: serverTimestamp(),
             });
 
             setIsSubmitting(false);
             setError('');
             console.log("Form data submitted successfully :", vehicleDocRef.id);
-    
+
+            const collectionId = adsCollectionRef.id;
             const docId = vehicleDocRef.id;
-            const collectionId = vehiclesCollection.id;
-    
             setCollectionId(collectionId);
             setDocumentId(docId);
-            
-            
-            nextStep(collectionId, docId);
+
+
+
+            nextStep(selectedCategory, collectionId, docId);
 
         } catch (error) {
             setIsSubmitting(false);
@@ -125,7 +178,7 @@ function VehiclesForm({ nextStep }){
 
     return (
         <div>
-            <form onSubmit={handleSubmit} className="flex flex-col w-full gap-y-4 mt-6">
+            <form onSubmit={''} className="flex flex-col w-full gap-y-4 mt-6">
                 {/* Form fields and controls */}
                 <label className='w-full'>
                     <p className='text-sm text-gray-800 mb-1'>
@@ -207,6 +260,19 @@ function VehiclesForm({ nextStep }){
                     </label>
                     <label className='w-full'>
                         <p className='text-sm text-gray-800 mb-1'>
+                            Kilometer Traveled <sup className='text-pink-200'>*</sup>
+                        </p>
+                        <input
+                            required
+                            type="number"
+                            value={totalkm}
+                            onChange={handletotalKmChange}
+                            placeholder="Enter Total Kilometer"
+                            className='bg-gray-100 rounded-md text-gray-800 w-full px-4 py-2'
+                        />
+                    </label>
+                    <label className='w-full'>
+                        <p className='text-sm text-gray-800 mb-1'>
                             Negotiable <sup className='text-pink-200'>*</sup>
                         </p>
                         <select
@@ -260,17 +326,60 @@ function VehiclesForm({ nextStep }){
                         <button onClick={handleAddTag} type="button" className="bg-blue-500 text-white px-4 py-2 rounded-md ml-2">
                             Add
                         </button
-                    ></div>
+                        ></div>
                 </label>
+                <hr></hr>
+
+
+                <lable>Add Images</lable>
+                {/* <div className="flex flex-wrap gap-4">
+                    {[...Array(6)].map((_, index) => (
+                        <label key={index} className="w-1/2 sm:w-1/3">
+                            <p className="text-sm text-gray-800 mb-1">Image {index + 1}:</p>
+                            <input
+                                type="file"
+                                accept="image/*"
+                                onChange={(e) => handleImageChange(e, index)}
+                                className="bg-gray-100 rounded-md text-gray-800 w-full px-4 py-2"
+                            />
+                            {images[index] && (
+                                <img src={images[index]} alt={`Preview ${index + 1}`} className="mt-2 w-full h-auto object-cover" />
+                            )}
+                        </label>
+                    ))}
+                </div> */}
+
+                <div className="flex flex-wrap gap-4">
+                    {[...Array(6)].map((_, index) => (
+                        <label key={index} className="w-1/2 sm:w-1/3">
+                            <p className="text-sm text-gray-800 mb-1">Image {index + 1}:</p>
+                            <input
+                                type="file"
+                                accept="image/*"
+                                onChange={(e) => handleImageChange(e, index)}
+                                className="bg-gray-100 rounded-md text-gray-800 w-full px-4 py-2"
+                            />
+                            {imageLoadings[index] && <div className="flex justify-center items-center">
+                                <div className="border-t-transparent border-solid animate-spin rounded-full border-blue-400 border-4 h-8 w-8"></div>
+                            </div>}
+                            {!imageLoadings[index] && images[index] && (
+                                <img src={images[index]} alt={`Preview ${index + 1}`} className="mt-2 w-full h-auto object-cover" />
+                            )}
+                        </label>
+                    ))}
+                </div>
+
+
+                <hr></hr>
 
                 <div className='flex flex-col-reverse sm:flex-row justify-between items-baseline'>
                     <p className='text-[#636A80]'> <Checkbox className='m-2'></Checkbox>Save my contact information for faster posting</p>
                     <div className='flex sm:flex-row gap-4'>
-                        <button type="button" className='border-4 rounded-md text-gray-800 h-14 w-36 sm:w-40 font-semibold py-2 mt-6'>
-                            Previous
+                    <button onClick={handlePreviousClick} className='border-4 rounded-md text-gray-800 h-14 w-36 sm:w-40 font-semibold py-2 mt-6'>
+                            previous
                         </button>
                         <button
-                            onClick={handleSubmit}
+                            onClick={handleNextClick}
                             className="border-4 bg-green-500 rounded-md text-gray-800 h-14 w-36 sm:w-40 font-semibold py-2 mt-6"
                         >
                             Next →
