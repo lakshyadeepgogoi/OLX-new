0323956d5e0dbc257897551eb731d61c7f6c08c6
lakshyadeepgogoi <lakshyadeep19@gmail.com>
2024-05-13 16:23:07 +0530

    update in formsuccess

--- src/components/postAds/FormSuccess.js
+++ src/components/postAds/FormSuccess.js
@@ -1,28 +1,26 @@
-import React from 'react'
+import React from 'react';
 import { FaArrowRightLong } from "react-icons/fa6";
-import check from "../../assets/circle-wavy-check-duotone 1.png"
-
+import check from "../../assets/circle-wavy-check-duotone 1.png";
 
 function FormSuccess() {
   return (
-    <div>
-        <div className="flex flex-col w-full gap-y-4 mt-6 py-16 items-center justify-center">
-                    <img src={check}></img>
-                    <h1 className="text-4xl font-bold text-center">Your ad is successfully published</h1>
-                    <p className="text-center text-xl mt-2 mb-6">Boost Add to sell faster by reaching more potential buyers</p>
-                    {/* Placeholder for step 3 confirmation */}
-                    <div className='flex flex-row gap-4'>
-                    <button className='border-4 rounded-md text-[#00AAFF] h-14 w-32 sm:w-40 font-semibold py-2 mt-6'>
-                    View Ad
-                    </button>
-
-                    <button className='bg-green-500 rounded-md text-gray-800 h-14 w-40 font-semibold py-2 mt-6 flex justify-center items-center gap-2' onClick={''}>
-                    Boost Ad <FaArrowRightLong/>
-                    </button>
-                    </div>
+    <div className="bg-gray-100 p-5 flex  justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-screen-md">
+        <img src={check} alt="check mark" className="mx-auto" />
+        <h1 className="text-4xl font-bold text-center mt-6">Your ad is successfully published</h1>
+        <p className="text-center text-lg mt-2 mb-6">Boost your ad to sell faster by reaching more potential buyers.</p>
+        <div className="flex flex-col md:flex-row justify-center items-center gap-4">
+          <button className="border border-blue-500 rounded-md text-blue-500 hover:bg-blue-500 hover:text-white h-12 md:h-14 w-full md:w-auto font-semibold py-2 mt-6 md:mt-0 transition duration-300 ease-in-out transform hover:scale-105 flex items-center justify-center">
+            <span className="mr-2">View Ad</span>
+          </button>
+          <button className="bg-green-500 rounded-md text-white hover:bg-green-600 h-12 md:h-14 w-full md:w-auto font-semibold py-2 mt-4 md:mt-0 flex items-center justify-center gap-2 transition duration-300 ease-in-out transform hover:scale-105">
+            <span>Boost Ad</span>
+            <FaArrowRightLong className="ml-1" />
+          </button>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default FormSuccess
+export default FormSuccess;
